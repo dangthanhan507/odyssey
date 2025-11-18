829c2e97da0a1e9c772069512719f7a539425d13
dangthanhan507 <dangthanhan507@gmail.com>
2025-11-17 19:01:58 -0500

    Made using viser optional for workstation.

--- odyssey/workstation.py
+++ odyssey/workstation.py
@@ -90,46 +90,47 @@ class OdysseyBaseWorkstation:
     def __init__(self, 
                  robot_urdf_path,
                  robot_description_path,
+                 use_viser=True,
                  load_meshes=True,
                  load_collision_meshes=False,
                  host='127.0.0.1',
                  port=8080):
-
-        # setup viser with URDF
-        self.server = viser.ViserServer(host=host, port=port)
-        
+        self.use_viser = use_viser
         self.kuka_lcm = KukaLCM()
-        
-        urdf = yourdfpy.URDF.load(
-            robot_urdf_path,
-            mesh_dir=robot_description_path,
-            load_meshes=load_meshes,
-            build_scene_graph=load_meshes,
-            load_collision_meshes=load_collision_meshes,
-            build_collision_scene_graph=load_collision_meshes,
-        )
-        self.viser_urdf = ViserUrdf(
-            self.server,
-            urdf_or_path=urdf,
-            load_meshes=load_meshes,
-            load_collision_meshes=load_collision_meshes,
-            collision_mesh_color_override=(1.0, 0.0, 0.0, 0.5),
-        )
-        
-        trimesh_scene = self.viser_urdf._urdf.scene or self.viser_urdf._urdf.collision_scene
-        self.server.scene.add_grid(
-            "/grid",
-            width=2,
-            height=2,
-            position=(
-                0.0,
-                0.0,
-                # Get the minimum z value of the trimesh scene.
-                trimesh_scene.bounds[0, 2] if trimesh_scene is not None else 0.0,
-            ),
-        )
-        self.viser_urdf.show_visual = True
-        self.viser_urdf.show_collision = True
+        if use_viser:
+            # setup viser with URDF
+            self.server = viser.ViserServer(host=host, port=port)
+            
+            urdf = yourdfpy.URDF.load(
+                robot_urdf_path,
+                mesh_dir=robot_description_path,
+                load_meshes=load_meshes,
+                build_scene_graph=load_meshes,
+                load_collision_meshes=load_collision_meshes,
+                build_collision_scene_graph=load_collision_meshes,
+            )
+            self.viser_urdf = ViserUrdf(
+                self.server,
+                urdf_or_path=urdf,
+                load_meshes=load_meshes,
+                load_collision_meshes=load_collision_meshes,
+                collision_mesh_color_override=(1.0, 0.0, 0.0, 0.5),
+            )
+            
+            trimesh_scene = self.viser_urdf._urdf.scene or self.viser_urdf._urdf.collision_scene
+            self.server.scene.add_grid(
+                "/grid",
+                width=2,
+                height=2,
+                position=(
+                    0.0,
+                    0.0,
+                    # Get the minimum z value of the trimesh scene.
+                    trimesh_scene.bounds[0, 2] if trimesh_scene is not None else 0.0,
+                ),
+            )
+            self.viser_urdf.show_visual = True
+            self.viser_urdf.show_collision = True
     
     def update_robot_joints(self, joint_positions):
         self.viser_urdf.update_cfg(np.array(joint_positions))
@@ -137,7 +138,7 @@ class OdysseyBaseWorkstation:
     def handle(self, ms = 0):
         self.kuka_lcm.handle(ms=ms)
         joint_position = self.kuka_lcm.get_joint_position_measured()
-        if joint_position is not None:
+        if self.use_viser and joint_position is not None:
             self.update_robot_joints(joint_position)
 
     def send_joint_command(self, joint_positions, torque = np.zeros(7)):
